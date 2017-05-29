91c8a2ecccddfb0198008cf9d62fc9ef11d8e370
asymme <pepel.on.tino+github@gmail.com>
2017-05-29 23:57:17 +0900

    [fix]It didn't work.

--- mastodon.user.js
+++ mastodon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube on Mastodon timeline
 // @namespace    https://github.com/asymme/
-// @version      0.1
+// @version      0.2.0
 // @description  You can watch youtube videos on Mastodon's timeline
 // @author       Asymme
 // @match        https://
@@ -10,31 +10,34 @@
 
 (function() {
     'use strict';
-    function loop() {
-        var contentList = $('.status__content');
-        var width = contentList.width();
-        var height = parseInt(width * 9 / 16);
-        contentList.each(function(outIdx, outElem) {
-            var content = $(outElem);
-            var a = content.find('a');
-            if(a.length === 0 || content.find('.ytVideo').length > 0) {
-                return true;
+    var mo = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if(mutation.type !== 'childList') { return; }
+
+            for(var i = 0; i < mutation.addedNodes.length; i++) {
+                var node = mutation.addedNodes[i];
+                if(node.querySelectorAll) { embedYouTube(node); }
             }
-            a.each(function(inIdx, inElem) {
-                var matches = $(inElem).attr('href').match(/https:\/\/(www|m)?\.?youtu\.?be(\.com)?\/(watch\?.*v=)?([-\w]+)/);
-                if(matches === null) {
-                    return true;
-                }
-                content.append( $('<iframe>').attr({
-                    'src': 'https://www.youtube.com/embed/' + matches[4],
-                    'width': width,
-                    'height': height,
-                    'frameborder': '0',
-                    'class': 'ytVideo'
-                }) );
-            });
         });
-        setTimeout(loop, 5000);
+    });
+    mo.observe(document, {
+        'childList': true,
+        'subtree': true
+    });
+
+    function embedYouTube(node) {
+        var statuses = node.querySelectorAll('.status__content > p > a');
+        for(var i = 0; i < statuses.length; i++) {
+            var matches = statuses[i].href.match(/https:\/\/(www|m)?\.?youtu\.?be(\.com)?\/(watch\?.*v=)?([-\w]+)/);
+            if(matches) {
+                var statusContent = statuses[i].parentNode.parentNode;
+                var iframe = document.createElement('iframe');
+                iframe.src = 'https://www.youtube.com/embed/' + matches[4];
+                iframe.width = statusContent.clientWidth;
+                iframe.height = '' + ((parseInt(iframe.width) * 9 / 16) | 0);
+                iframe.frameBorder = '0';
+                statusContent.appendChild(iframe);
+            }
+        }
     }
-    loop();
 })();
